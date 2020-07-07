430464017f9820dea479a3380ff9df4afd66c49f
flowerinthenight <flowerinthenight@pm.me>
2020-07-07 13:39:32 +0900

    iface, gomod

--- dlock.go
+++ dlock.go
@@ -1,1 +1,8 @@
 package dlock
+
+import "context"
+
+type Locker interface {
+	Lock(ctx context.Context) error
+	Unlock() error
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/flowerinthenight/dlock
+
+go 1.14
